c95d6057f67efe8dc1ce077df46fddf205630cca
xycode <h.xiongyu@gmail.com>
2020-04-10 14:16:07 +0800

    publish ui 脚本

--- ui/trans_ui_to_py.py
+++ ui/trans_ui_to_py.py
@@ -18,6 +18,17 @@ def list_ui_file():
             ls.append(filename)
     return ls
 
+def list_qrc_file():
+    """
+    列出目录下的所有.ui文件
+    :return: 文件名的list
+    """
+    ls = []
+    files = os.listdir(file_dir)
+    for filename in files:
+        if os.path.splitext(filename)[1] == '.qrc':
+            ls.append(filename)
+    return ls
 
 def trans_py_file(filename):
     """
@@ -27,6 +38,13 @@ def trans_py_file(filename):
     """
     return "ui_%s.py"%(os.path.splitext(filename)[0])
 
+def trans_py_file2(filename):
+    """
+    转换.ui扩展名到.py
+    :param filename: 。ui文件名
+    :return: .py文件名
+    """
+    return "%s_rc.py"%(os.path.splitext(filename)[0])
 
 def run():
     """
@@ -34,14 +52,20 @@ def run():
     :return: None
     """
     file_list = list_ui_file()
-    print(file_list)
     for ui_file in file_list:
         py_file = trans_py_file(ui_file)
         cmd = 'pyuic5 -x {ui_dir}/{ui_file} -o {ui_dir}/{py_file}' \
             .format(ui_file=ui_file, py_file=py_file, ui_dir=file_dir)
         print(cmd)
         os.system(cmd)
-
+    
+    qrcfiles = list_qrc_file()
+    for qrc_file in qrcfiles:
+        py_file = trans_py_file2(qrc_file)
+        cmd = 'pyrcc5 {ui_dir}/{ui_file} -o {ui_dir}/{py_file}' \
+            .format(ui_file=qrc_file, py_file=py_file, ui_dir=file_dir)
+        print(cmd)
+        os.system(cmd)
 
 if __name__ == "__main__":
     import sys
